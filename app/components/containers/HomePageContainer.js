45ed7748a8828380b69d396c9d65debd954ff95d
clarkngo <clarkngo@gmail.com>
2018-11-18 02:11:28 -0800

    work

--- app/components/containers/HomePageContainer.js
+++ app/components/containers/HomePageContainer.js
@@ -1,10 +1,48 @@
 import React, { Fragment } from 'react';
+import Card from '../Card';
+import GridList from '../GridList';
+import { withStyles } from '@material-ui/core/styles';
+import FloatingActionButtonZoom from '../TabContainer';
+import ButtonBases from '../ButtonBases';
 
+
+const styles = theme => ({
+  flex: {
+    display: 'flex',
+  },
+});
 
 /* The component that shows the home page containt */
-export const HomePageContainer = () => (
+export const HomePageContainer = ({ classes }) => (
   <Fragment>
-    Content goes here!
+        <br />
+    <br />
+    <ButtonBases></ButtonBases>
+    <GridList />
+    <div className={classes.flex}>
+      <Card
+        image="https://s7d3.scene7.com/is/image/LaCrosse/37440?$lacrosse_large_thumb_2x$"
+        title="Hiking Boots"
+        desc="Hiking Boots are essential"
+      />
+      <Card
+        image="https://s7d3.scene7.com/is/image/LaCrosse/37440?$lacrosse_large_thumb_2x$"
+        title="Hiking Boots"
+        desc="Hiking Boots are essential"
+      />
+      <Card
+        image="https://s7d3.scene7.com/is/image/LaCrosse/37440?$lacrosse_large_thumb_2x$"
+        title="Hiking Boots"
+        desc="Hiking Boots are essential"
+      />            
+    </div>  
+    <FloatingActionButtonZoom>
+    <Card
+        image="https://s7d3.scene7.com/is/image/LaCrosse/37440?$lacrosse_large_thumb_2x$"
+        title="Hiking Boots"
+        desc="Hiking Boots are essential"
+      />
+    </FloatingActionButtonZoom>
   </Fragment>
 );
-export default HomePageContainer;
+export default withStyles(styles)(HomePageContainer);
